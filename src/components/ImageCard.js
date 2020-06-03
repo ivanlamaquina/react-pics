a87352e31e7f03aed0181397ecb5a4cb193d3eb0
Iván Palacios <ivanlamaquina@gmail.com>
2020-06-03 15:07:05 +0200

    Commit inicial. Segun se termino en el curso al final de la seccion 10

---
+++ src/components/ImageCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // Se añade un evento para que cargue la altura de la imagen
+    // cuando se haya cargado
+    this.imageRef.current.addEventListener('load', this.setImageSpans);
+  }
+
+  setImageSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height) + 5;
+
+    this.setState({ spans });
+  };
+
+  render() {
+    const { description, urls } = this.props.image;
+    return (
+      <div
+        style={{
+          gridRowEnd: `span ${this.state.spans}`,
+          margin: 'auto'
+        }}
+      >
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
